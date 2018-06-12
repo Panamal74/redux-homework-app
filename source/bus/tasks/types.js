9b374d749ba8fccc296837e82c279475b30bc217
Panamal74 <pavlo.malakhovskyi@gmail.com>
2018-06-12 17:10:35 +0300

    Нужно разобраться с FETCH_TASKS

--- source/bus/tasks/types.js
+++ source/bus/tasks/types.js
@@ -1,14 +1,14 @@
 export const types = Object.freeze({
     // fetch
-    FETCH_TASKS:         'FETCH_TASKS',
+    FETCH_TASKS: 'FETCH_TASKS',
     // FETCH_TASKS_SUCCESS: 'FETCH_TASKS_SUCCESS',
     // FETCH_TASKS_FAIL:    'FETCH_TASKS_FAIL',
     // create
-    CREATE_TASK:         'CREATE_TASK',
+    CREATE_TASK: 'CREATE_TASK',
     // remove
-    REMOVE_TASK:         'REMOVE_TASK',
+    REMOVE_TASK: 'REMOVE_TASK',
     // change
-    CHANGE_TASK:         'CHANGE_TASK',
+    CHANGE_TASK: 'CHANGE_TASK',
     // // like
     // LIKE_TASK:           'LIKE_TASK',
     // UNLIKE_TASK:         'UNLIKE_TASK',
